2f968a71cb135e9ebd278fe2624ccf9f331e5b0f
Arthur César <57773827+arthurgit1@users.noreply.github.com>
2025-08-02 01:45:21 -0300

    config tailwind

---
+++ gestao-processos/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"  // Garante que Tailwind leia os arquivos Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
