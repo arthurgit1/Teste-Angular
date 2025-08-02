d0dd6234d8003a28eb469263af5409c07665b8a0
Arthur César <57773827+arthurgit1@users.noreply.github.com>
2025-08-02 07:17:27 -0300

    Update dashboard.module.ts

--- gestao-juridica/src/app/features/dashboard/dashboard.module.ts
+++ gestao-juridica/src/app/features/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+// Verifique se o caminho de importação está correto (./dashboard/dashboard.component)
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
-    DashboardComponent  // O componente é declarado aqui
+    DashboardComponent 
   ],
   imports: [
     CommonModule,
