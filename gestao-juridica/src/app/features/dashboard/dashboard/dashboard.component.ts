e26eac89aab030242d56a20addb1cad4cfddef02
Arthur César <57773827+arthurgit1@users.noreply.github.com>
2025-08-02 07:05:40 -0300

    Update dashboard.component.ts

--- gestao-juridica/src/app/features/dashboard/dashboard/dashboard.component.ts
+++ gestao-juridica/src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
+// src/app/features/dashboard/dashboard.component.ts
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
-})
+  styleUrls: ['./dashboard.component.css']})
+
 export class DashboardComponent implements OnInit {
 
   // ... o restante do seu código aqui
